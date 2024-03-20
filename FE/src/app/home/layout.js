307a7e465293e52b3c843ac0eeb28b1cb1070c3d
KhangNguyen3008 <134484883+KhangNguyen3008@users.noreply.github.com>
2024-03-20 12:15:49 +0700

    update homepage

--- FE/src/app/home/layout.js
+++ FE/src/app/home/layout.js
@@ -1,6 +1,7 @@
 
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import Navbar from "@/components/layout/Navbar";
 import Home from "./page";
 
 
@@ -12,7 +13,7 @@ const metadata = {
 export default function Layout({ children }) {
   return (
     <>
-
+    <Navbar/>
       <Home />
       <Footer />
     </>
