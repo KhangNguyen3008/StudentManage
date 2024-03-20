793ac9bd30ce9b228b7ce0c7042a50ff848949fd
KhangNguyen3008 <134484883+KhangNguyen3008@users.noreply.github.com>
2024-03-20 12:34:02 +0700

    update homepage and fix navbar

--- FE/src/app/home/layout.js
+++ FE/src/app/home/layout.js
@@ -1,5 +1,4 @@
 
-import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
 import Home from "./page";
--- FE/src/app/page.js
+++ FE/src/app/page.js
@@ -1,9 +1,12 @@
 import Home from "@/components/pages/Home";
+import Navbar from "@/components/layout/Navbar";
+
 import Image from "next/image";
 
 export default function page() {
   return (
    <div>
+      <Navbar/>
       <Home/>
    </div>
   );
