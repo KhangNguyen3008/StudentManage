ad4c397c047b6a17dbcdbea9706e349475999e3a
tài võ <libra13576@gmail.com>
2024-03-29 11:27:53 +0700

    fix index

---
+++ FE/src/app/page/Index/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../globals.css';
+
+function Index() {
+  return (
+    <>
+  <div className="page">
+
+  </div>
+    </>
+  )
+}
+
+export default Index
--- FE/src/app/page/NewArticle/layout.js
+++ FE/src/app/page/NewArticle/layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from "@/components/layout/Footer";
-import NewArtical from '@/components/pages/NewArticle';
 import Navbar from '@/components/layout/Navbar';
+import NewArticle from '@/components/pages/NewArticle';
 
 const metadata = {
   title: "Create Next App",
@@ -12,7 +12,7 @@ export default function Layout({ children }) {
   return (
       <>
         <Navbar/> 
-          <NewArtical/>
+          <NewArticle/>
           <Footer/>
       </>
   );
