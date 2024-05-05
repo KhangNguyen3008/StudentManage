9f9d9a80b74c1723c04e2b6ed5f15cba20d9b6b3
tài võ <libra13576@gmail.com>
2024-05-05 12:59:21 +0700

    fix home

--- FE/src/app/guest/page.jsx
+++ FE/src/app/guest/page.jsx
@@ -1,8 +1,10 @@
 import GuestIndex from '@/components/pages/GuestIndex'
 import React from 'react'
+import Homeguest from '@/components/layout/Homeguest'
 
 export default function page() {
   return (
-    <div><GuestIndex/></div>
+    <div>    <Homeguest/>
+    </div>
   )
 }
